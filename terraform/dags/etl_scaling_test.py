ccac17e0a7d05ad4a40fca081e000aa6aab90db4
Vigtus <1kapiolk@wp.pl>
2026-01-11 22:51:45 +0100

    Fix DAG for tests2

--- terraform/dags/etl_scaling_test.py
+++ terraform/dags/etl_scaling_test.py
@@ -15,7 +15,7 @@ with DAG(
     start_date=datetime(2024, 1, 1),
     schedule=None,
     catchup=False,
-    max_active_runs=1,
+    max_active_runs=2,
     tags=["scaling", "keda", "test"],
 ) as dag:
 
